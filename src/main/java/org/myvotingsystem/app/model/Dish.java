c763815d6812fdf8d4df6fbaaf56a0572e1b6e46
lambenteng <lambenteng@gmail.com>
2020-05-06 01:25:00 +0300

    public static Strings, Menu getter/setter and constructor field added

--- src/main/java/org/myvotingsystem/app/model/Dish.java
+++ src/main/java/org/myvotingsystem/app/model/Dish.java
@@ -1,17 +1,29 @@
 package org.myvotingsystem.app.model;
 
 public class Dish extends BaseEntity {
+
+    public static final String GET_ALL = "Dish.getAll";
+    public static final String GET_ALL_MENU_DISHES = "Dish.getAllMenuDishes";
+    public static final String DELETE = "Dish.delete";
+    public static final String DELETE_ALL_MENU_DISHES = "Dish.deleteAllMenuDishes";
+
     private String name;
     private double price;
+    private Menu menu;
 
     public Dish(int id, String name, double price) {
         super(id);
         this.name = name;
         this.price = price;
+        this.menu = null;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
     }
 
     public void setName(String name) {
@@ -33,5 +45,9 @@ public class Dish extends BaseEntity {
                 ", name='" + name +
                 ", price=" + price +
                 '}';
+    }
+
+    public Menu getMenu() {
+        return menu;
     }
 }
