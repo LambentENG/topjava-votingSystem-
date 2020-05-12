954437dbbcf3ecf2db65b90dd485697502c1191b
lambenteng <lambenteng@gmail.com>
2020-05-13 01:37:31 +0300

    AbstractServiceTest added

---
+++ src/test/java/org/myvotingsystem/app/service/implementations/AbstractServiceTest.java
@@ -0,0 +1,18 @@
+package org.myvotingsystem.app.service.implementations;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.myvotingsystem.app.config.AppConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = {AppConfig.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+abstract public class AbstractServiceTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+}
